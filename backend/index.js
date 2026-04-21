1ef27f2b3499af7cb25ae6a1046499bd903e687a
ROBERTO ANDRES THOMAS TORRES <rtt2594@gmail.com>
2026-04-21 17:21:15 -0600

    update: nueva ruta crear evento

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,14 @@ const PORT = process.env.PORT || 3000;
 // Importación de rutas
 const usuarioRoute = require("./routes/usuario.route");
 const inscripcionRoute = require("./routes/inscribirme.route");
+const crearEventoRoute = require("./routes/crearEvento.route");
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cors());
+app.use('/uploads', express.static('uploads'));
+
 
 require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]); // Para el error querySrv ECONNREFUSED  
 
@@ -25,6 +28,7 @@ mongoose.connect(process.env.MONGODB_URI)
 // Rutas
 app.use("/usuario", usuarioRoute);
 app.use("/inscribirme", inscripcionRoute);
+app.use("/crearEvento", crearEventoRoute);
 
 app.get('/', (req,res)=> {
     res.send('Servidor en funcionamiento');
