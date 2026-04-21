f48d35f61c1e6e71c13afe723880dbd0b3e9075d
ROBERTO ANDRES THOMAS TORRES <rtt2594@gmail.com>
2026-04-21 17:17:49 -0600

    new: route de inscribirme a evento

--- backend/routes/inscribirme.route.js
+++ backend/routes/inscribirme.route.js
@@ -24,4 +24,95 @@ router.post("/", async (req, res) => {
     }
 });
 
+
+// Ver todas las inscripciones
+router.get("/", async (req, res) => {
+    try {
+        const usuarios = await Inscribirme.find();
+        res.json(usuarios);
+
+    } catch (error) {
+        res.status(500).json({ msg: "Error al obtener inscripciones" });
+    }
+});
+
+
+// Ver por ID
+router.get("/:id", async (req, res) => {
+    try {
+        const inscribirme = await Inscribirme.findById(req.params.id);
+
+        if (!inscribirme) {
+            return res.status(404).json({ msg: "Inscripcion no encontrado" });
+        }
+
+        res.json(inscribirme);
+
+    } catch (error) {
+        res.status(500).json({ msg: "Error al buscar inscripcion" });
+    }
+});
+
+
+// PUT
+router.put("/:id", async (req, res) => {
+    try {
+        const inscripcionActualizado = await Inscribirme.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true, runValidators: true }
+        );
+
+        if (!inscripcionActualizado) {
+            return res.status(404).json({ msg: "Inscripción no encontrado" });
+        }
+
+        res.json(inscripcionActualizado);
+
+    } catch (error) {
+        res.status(500).json({ msg: "Error al actualizar inscripción" });
+    }
+});
+
+
+// Actualizar estado
+router.patch("/:id/estado", async (req, res) => {
+    try {
+        const { role } = req.body;
+
+        const inscribirme = await Inscribirme.findByIdAndUpdate(
+            req.params.id,
+            { role },
+            { new: true, runValidators: true }
+        );
+
+        if (!inscribirme) {
+            return res.status(404).json({ msg: "Inscripción no encontrada" });
+        }
+
+        res.json(inscribirme);
+
+    } catch (error) {
+        res.status(500).json({ msg: "Error al actualizar estado de inscripción" });
+    }
+});
+
+
+// Eliminar
+router.delete("/:id", async (req, res) => {
+    try {
+        const inscripcionEliminada = await Inscribirme.findByIdAndDelete(req.params.id);
+
+        if (!inscripcionEliminada) {
+            return res.status(404).json({ msg: "Inscripción no encontrada" });
+        }
+
+        res.json({ msg: "Inscripcion eliminada correctamente" });
+
+    } catch (error) {
+        res.status(500).json({ msg: "Error al eliminar inscripción" });
+    }
+});
+
+
 module.exports = router;
